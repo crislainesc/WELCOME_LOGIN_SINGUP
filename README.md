e646845a344b4beceaa9c60edd5795352fb00758
Crislâine Santos <crislaine.costa@arapiraca.ufal.br>
2021-09-03 17:51:39 -0300

    Flutter

---
+++ README.md
@@ -0,0 +1,13 @@
+# Welcome, Login e SingUp
+
+Um projeto em flutter com uma três páginas:
+
+- Welcome: Página Inicial,
+
+- Login: Página para realizar login,
+
+- SingUp: Página de cadastro.
+
+
+Desenvolvido seguindo o tutorial
+[Welcome, Login, Signup Page - Flutter UI - Speed Code](https://www.youtube.com/watch?v=ExKYjqgswJg), feito por [Abu Anwar](https://github.com/abuanwar072).
---
+++ lib/components/rounded_input_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/components/text_field_container.dart';
+import 'package:flutter_auth/constants.dart';
+
+class RoundedInputField extends StatelessWidget {
+  final String hinText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedInputField({
+    Key key,
+    this.hinText,
+    this.icon = Icons.person,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hinText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
